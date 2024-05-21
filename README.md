a9469a1a9a1b50c2e921f60ba5c8848ebb314c2d
Kseniya Freeflyer <kalyakina.ks.a@gmail.com>
2024-05-21 14:55:15 +0300

    add brain-even.js

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Ksuniqum23/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Ksuniqum23/frontend-project-44/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/67e58e054616d181905a/maintainability)](https://codeclimate.com/github/Ksuniqum23/frontend-project-44/maintainability)
+https://asciinema.org/a/rReAtr09hOncu9pU20wEMMg2f
+
---
+++ bin/brain-even.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const generateValue = () => {
+    return Math.floor(Math.random() * 50);
+}
+
+import readlineSync from 'readline-sync';
+import getName from '../src/cli.js';
+
+const name = getName();
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+for (let i = 0; i < 3; i++) {
+    const value = generateValue();
+    console.log(`Question: ${value}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if ((value % 2 === 0 && answer === 'yes') || (value % 2 !== 0 && answer === 'no')) {
+        console.log('Correct!');
+    } else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'` );
+        console.log(`Let's try again, ${name}!`);
+        break;
+    }
+
+    if (i === 2) {
+        console.log(`Congratulations, ${name}!`);
+    }
+}
