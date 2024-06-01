8871b31c43fee125c1b014e2332c1cd6322c516a
Kseniya Freeflyer <kalyakina.ks.a@gmail.com>
2024-06-01 18:24:01 +0300

    fix brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
 import getName from '../src/cli.js';
 
 const name = getName();
+
+console.log('Welcome to the Brain Games!');
+console.log(`Hello, ${name}!`);
